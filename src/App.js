42dd16c4800e18c6c1255ffc3cca3973d57d40c6
diapoor <trimvaobongtoi@gmail.com>
2024-05-20 02:01:23 +0700

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './Home'; // Đảm bảo bạn đã nhập đúng tên file
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
