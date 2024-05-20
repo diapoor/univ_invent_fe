e4cd3a307f232cb293e2328bce85f83f004cbc22
diapoor <trimvaobongtoi@gmail.com>
2024-05-20 14:05:41 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './Home'; // Đảm bảo bạn đã nhập đúng tên file
+import AppBar from './components/Appbar';
+import userView from './components/user/userView';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <userView />
     </div>
   );
 }
