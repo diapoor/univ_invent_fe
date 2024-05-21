7b36c3deccb8e2d68269ab5a4f2075b8d7610d7d
diapoor <trimvaobongtoi@gmail.com>
2024-05-21 19:25:29 +0700

    fix footer

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './Footer.css'; // Import file CSS của bạn
 
 function Footer() {
   return (
-    <footer className="footer-container">
-      <div className="footer-content">
-        <p>Inventory Management System &copy; 2024</p>
-        <p>Contact: Trimvaobongtoi@gmail.com</p>
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2024 Your Website. All rights reserved.</p>
+        <div className="social-links">
+          <a href="#"><i className="fa fa-facebook"></i></a>
+          <a href="#"><i className="fa fa-twitter"></i></a>
+          <a href="#"><i className="fa fa-instagram"></i></a>
+          <a href="#"><i className="fa fa-linkedin"></i></a>
+        </div>
       </div>
     </footer>
   );
